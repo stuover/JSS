d03b0919d6d4770575eaaadd9b249bf3f356c7f3
YOUNG <ddi061205@naver.com>
2023-05-15 19:31:30 +0900

    230515

---
+++ japsswa/src/main/java/com/mes/jss/sale/domain/CustomerVO.java
@@ -0,0 +1,19 @@
+package com.mes.jss.sale.domain;
+
+import java.util.Date;
+
+public class CustomerVO {
+	String customerId;
+	String customerName;
+	String custAddr;
+	String custMail;
+	String custTel;
+	String custPart;
+	String contractStatus;
+	String custEtc;
+	int empNo;
+	
+	String contractId;
+	String contProd;
+	Date contDate;
+}
---
+++ japsswa/src/main/java/com/mes/jss/sale/domain/OrderVO.java
@@ -0,0 +1,25 @@
+package com.mes.jss.sale.domain;
+
+import java.util.Date;
+
+public class OrderVO {
+	String ordId;
+	String ordName;
+	int totalPrice;
+	String ordStatus;
+	String customerId;
+	Date ordDate;
+	int empNo;
+	
+	String ordDetailId;
+	int ordCount;
+	Date outDate;
+	String itemCode;
+	
+	String customerName;
+	String custAddr;
+	String custMail;
+	String custTel;
+	String itemName;
+	String itemPrice;
+}
