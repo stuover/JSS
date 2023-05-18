efc980617c62455d8b9e933249bb0db9dedd950e
admin <admin@YD05-12.mshome.net>
2023-05-18 09:58:23 +0900

    셀렉트박스

--- japsswa/src/main/java/com/mes/jss/facility/service/FacilityService.java
+++ japsswa/src/main/java/com/mes/jss/facility/service/FacilityService.java
@@ -12,5 +12,5 @@ public interface FacilityService {
 	List<FacilityVO> getList();	// 전체 설비 리스트
 	DowntimeVO getDowntime(DowntimeVO vo);
 	List<DowntimeVO> getDownList();	// 비가동 리스트
-	InspectionVO getInspection(InspectionVO vo);	
+	InspectionVO getInspection(InspectionVO vo);
 }
