dcca8b6a2c05c63b7e9d1ff02107a6a2288bcdc3
sunmo <stuover@nate.com>
2023-05-18 18:52:43 +0900

    로그인까지

--- japsswa/src/main/java/com/mes/jss/basic/serviceImpl/EmployeesServiceImpl.java
+++ japsswa/src/main/java/com/mes/jss/basic/serviceImpl/EmployeesServiceImpl.java
@@ -3,6 +3,9 @@ package com.mes.jss.basic.serviceImpl;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.mes.jss.DTO.Criteria;
@@ -12,10 +15,34 @@ import com.mes.jss.basic.service.EmployeesService;
 
 
 @Service("emploeesService")
-public class EmployeesServiceImpl implements EmployeesService{
+public class EmployeesServiceImpl implements EmployeesService, UserDetailsService{
 	
 	@Autowired EmployeesMapper mapper;
 
+	
+	@Override
+	public EmpVO gerUser(EmpVO vo) {
+
+		return mapper.getUser(vo);
+	}
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+		// 단건조회
+		EmpVO userVO = new EmpVO();
+		userVO.setEmpNo(username);
+		EmpVO vo =  mapper.getUser(userVO);		
+
+		// id 없으면 에러
+		if(vo == null) {
+			throw new UsernameNotFoundException("no id");
+		}
+
+		// id 있으면 vo return
+		return vo;		// Principal
+	}
+	
 	@Override
 	public List<EmpVO> getEmpList(){
 		return mapper.getList();
@@ -43,5 +70,20 @@ public class EmployeesServiceImpl implements EmployeesService{
 		// TODO Auto-generated method stub
 		return mapper.selectEmp(vo);
 	}
+
+	@Override
+	public boolean modifyEmp(EmpVO vo) {
+		return mapper.updateEmp(vo);
+	}
+
+	@Override
+	public boolean removeEmp(EmpVO vo) {
+		
+		return mapper.deleteEmp(vo);
+	}
+	
+	
+
+
 	
 }
